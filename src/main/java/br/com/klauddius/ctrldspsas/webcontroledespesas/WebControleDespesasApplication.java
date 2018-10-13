a291dbb5f1b26f535f65539a664f0ef8967782ce
Claudio Brito Moreno <klauddius@gmail.com>
2018-10-12 23:30:03 -0300

    Initial commit

---
+++ src/main/java/br/com/klauddius/ctrldspsas/webcontroledespesas/WebControleDespesasApplication.java
@@ -0,0 +1,12 @@
+package br.com.klauddius.ctrldspsas.webcontroledespesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebControleDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebControleDespesasApplication.class, args);
+	}
+}
